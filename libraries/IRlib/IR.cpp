f8c6f1f04a4856d9925becda73ae35976b566105
wchrisj <jansenchris833@gmail.com>
2019-11-22 19:00:26 +0100

    De library kan nu IR signalen ontvangen en decoden

--- libraries/IRlib/IR.cpp
+++ libraries/IRlib/IR.cpp
@@ -4,6 +4,8 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+IRsettings_t IRsettings;
+irparams_t irparams;
 /*
     Constructor
     stelt de zendpin in en stelt de frequent van de timer op deze pin in
@@ -12,15 +14,59 @@ IR::IR(uint8_t receivePin, uint8_t kHz){
     //Maak sender en receiver aan
     //Vul IRsettings
 
-    IRsettings.sendPin = receivePin;
-
-    IR::IRsettings.sendPin = (1 << PIND3); //Zet de zendpin als pin D3, deze hoort bij timer 2
-    IR::setupSendTimer(kHz);
-    IR::khz = kHz;
+    IRsettings.receivePin = receivePin; // Zet de receivePin
+    IRsettings.frequency = kHz;
+    IRsettings.sendPin = (1 << PIND3); //Zet de zendpin als pin D3, deze hoort bij timer 2
+    setupSendTimer(kHz);
 }
 
 void IR::enableReceiver(){
+    cli(); // Zet even alle interrupt uit zodat dit goed ingesteld kan worden
 
+    // Zet de ontvang pin goed
+    DDRB &= ~(1 << IRsettings.receivePin);
+    PORTB &= ~(1 << IRsettings.receivePin);
+
+    TCCR2A = (1 << WGM21);  // Zet de timer op CTC mode
+    TCCR2B = (1 << CS21);   // Zet de prescaler op 8
+    OCR2A  = TIMER_COUNT_TOP / 8;
+    TCNT2  = 0;
+
+    TIMSK2 = (1 << OCIE2A); // Zet timer interrupt aan
+
+    sei();  // Zet de interrupt weer aan
+
+    IR::resumeReceiver(); // Ga data inlezen
+}
+
+uint8_t IR::decode(){
+    if (irparams.receiveState != STOP){  //Dit check oP?
+        IR::resumeReceiver(); // Ga nieuwe data inlezen
+        return 0;
+    }
+
+    uint8_t dataBits = 0;
+    uint16_t data = 0;
+    for (int i = 1; i < irparams.rawlen; i++) { //Helft van de getallen zijn verkeerd.
+        if (!(i & 1)) { //We zijn alleen geinteresseerd in tijd dat het signaal LOW is
+            if (i == 1) {
+                //Start bit
+            }else if (irparams.rawbuf[i] * 50 <= MAX_ZERO_VALUE) {
+                dataBits++;
+            }else if (irparams.rawbuf[i] * 50 > MAX_ZERO_VALUE) {
+                data += (1 << dataBits);
+                dataBits++;
+            }
+        }
+    }
+    results.data = data;
+    results.dataBits = dataBits;
+    return 1;
+}
+
+void IR::resumeReceiver(){
+    irparams.receiveState = IDLE;
+    irparams.rawlen = 0;
 }
 
 void IR::setupSendTimer(uint8_t kHz){
@@ -59,7 +105,7 @@ void IR::send(uint16_t data, uint8_t len){
     TIMSK2 = 0;
     DDRD |= (1 << DDD3);
     PIND &= ~(1 << DDD3);
-    IR::setupSendTimer(IR::khz);
+    IR::setupSendTimer(IRsettings.frequency);
 
     //Zend start bit
     IR::sendStartBit();
@@ -105,4 +151,55 @@ void IR::sendLow(){
 
     TCCR2A &= ~(1 << COM2B1);
     _delay_us(TIME_LOW);
+}
+
+
+//Timer interrupt voor het opvangen van IR signalen
+ISR (TIMER2_COMPA_vect){
+    uint8_t  irdata = (PINB & (1 << IRsettings.receivePin)) >> IRsettings.receivePin; // Lees de data in van de ontvang pin, 0 -> signaal is HIGH, 1 -> signaal is LOW
+
+    irparams.timer++;  // Zet de timer 1 hoger, voor het meten van de tijd dat het signaal LOW is
+    if (irparams.rawlen >= RAWBUF)  irparams.receiveState = OVERFLOW ;  // Past de nieuwe data er niet meer bij -> Zet de status op OVERFLOW
+
+    switch(irparams.receiveState){
+        case IDLE: // Is de ontvanger nergens mee bezig
+            if (irdata == HIGH) { // Is er een verandering in het signaal
+                if (irparams.timer < GAP_TICKS)  {  // Is het signaal lang genoeg LOW geweest -> is dit geen ruis?
+                    irparams.timer = 0;                                 // Reset de timer
+                } else {
+                    irparams.rawlen++;                                      // Zet de lengte van de buffer +1
+                    irparams.overflow                = 0;                   // Zet overflow op false
+                    irparams.rawlen                  = 0;                   // Begin opnieuw met data in het buffer te plaatsen
+                    irparams.rawbuf[irparams.rawlen] = irparams.timer;  // Sla de tijd op in het buffer
+                    irparams.timer                   = 0;                   // Start de timer opnieuw
+                    irparams.receiveState            = MARK;                // Zet de status op het meten van signaal HIGH
+                }
+            }
+            break;
+        case MARK: // Wordt de tijd dat het signaal HIGH is gemeten
+            if (irdata == LOW) {   // Is het signaal niet meer HIGH -> sla dan de tijd op
+                irparams.rawbuf[irparams.rawlen++] = irparams.timer;    // Sla de tijd op
+                irparams.timer                     = 0;                     // Reset de timer zodat een nieuwe meting gedaan kan worden
+                irparams.receiveState              = SPACE;                 // Begin de signaal LOW meting
+            }
+            break;
+        case SPACE: // Wordt de tijd dat het signaal HIGH is gemeten
+            if (irdata == HIGH) {  // Is het signaal niet meer LOW -> sla dan de tijd op
+                irparams.rawbuf[irparams.rawlen++] = irparams.timer;    // Sla de tijd op
+                irparams.timer                     = 0;                     // Reset de timer zodat een nieuwe meting gedaan kan worden
+                irparams.receiveState              = MARK;                  // Begin de signaal HIGH meting
+            } else if (irparams.timer > GAP_TICKS) {  // Is het signaal lang LOW, dan wordt er geen data meer verstuurd -> zet de status op stop
+                irparams.receiveState = STOP;                               // Zet de status op STOP
+            }
+            break;
+        case STOP:  // Is het signaal ontvangen
+            if (irdata == HIGH){ // Is er verandering in het signaal
+                irparams.timer = 0 ;  // Reset de timer
+            }
+            break;
+        case OVERFLOW:  // Is het buffer vol?
+            irparams.overflow = 1;        // Zet overflow op true
+            irparams.receiveState = STOP; // Zet de status op STOP
+            break;
+    }
 }
--- libraries/IRlib/IR.h
+++ libraries/IRlib/IR.h
@@ -8,9 +8,52 @@
 #define TIME_1 1200
 #define TIME_START 2400
 
+#define HIGH 0 // Bij infrarood communicatie is het signaal 'HIGH', voor de leesbaarheid is het in deze library omgekeerd
+#define LOW  1
+
+#define USECPERTICK 50 // Hoeveel tijd zit er tussen de metingen?
+#define GAP_TICKS 100 // Hoeveel ticks mag het signaal achter elkaar LOW zijn, als er geen data verstuurt word is het signaal LOW
+#define MAX_ZERO_VALUE 900 // Wat is de hoogste waarde die een 0 mag hebben -> hoger is het een 1
+
+#define TIMER_COUNT_TOP  (F_CPU * USECPERTICK / 1000000) // Bij een prescaler van 0, wat is de top van de timer zodat er tussen 2 pulsen USECPERTICK tussen zit
+
+#define RECV_PIN PORTB3
+
+#define RAWBUF  101  // Maximale lengte van de buffer
+
+typedef
+    enum STATE{
+        IDLE,
+        MARK,     // Wordt de tijd dat het signaal HIGH is gemeten
+        SPACE,    // Wordt de tijd dat het signaal LOW is gemeten
+        STOP,     // Is de data ontvangen en klaar om gedecode te worden
+        OVERFLOW  // Is er meer data ontvangen dan er kan worden opgeslagen
+    }
+state_t;
+
+typedef
+    struct {
+        uint8_t  identifier;   // Hier staat voor wie de data is
+        uint16_t data;         // Hier staat de ontvangen data
+        uint8_t  dataBits;     // Hier staat de hoeveelheid dataBits -> geen startbit, geen paritybit
+        uint8_t  parityCheck;  // Hier staat als het paritybit klopt
+    }
+decoderesults_t;
+
+typedef
+    struct {
+        state_t       receiveState;    // Hier staat in welke status de ontvanger verkeerd
+        uint8_t       rawlen;          // De lengte van de buffer
+        unsigned int  timer;           // De timer 1 is gelijk aan USECPERTICK.
+        unsigned int  rawbuf[RAWBUF];  // De gemeette tijden
+        uint8_t       overflow;        // Is er een overflow?
+    }
+irparams_t;
+
 typedef
     struct{
         uint8_t receivePin;
+        uint8_t frequency;
         uint8_t sendPin;
     }
 IRsettings_t;
@@ -20,16 +63,16 @@ class IR{
         IR(uint8_t receivePin, uint8_t kHz);
         void send(uint16_t data, uint8_t len);
         void enableReceiver();
+        void resumeReceiver();
+        uint8_t decode();
 
-        IRsettings_t IRsettings;
+        decoderesults_t results;
 
     private:
         void setupSendTimer(uint8_t kHz);
         void sendStartBit();
         void sendHigh();
         void sendLow();
-
-        uint8_t khz;
 };
 
 #endif
