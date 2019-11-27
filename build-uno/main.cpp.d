231ed8c067ae81bc91c434d52ffe2bb1bed38264
Spookles <sim.brolsma@gmail.com>
2019-11-27 14:07:08 +0100

    Onnodige mapjes eruit gehaald d.m.v. gitignore

---
+++ build-uno/main.cpp.d
@@ -0,0 +1,8 @@
+build-uno/./main.cpp.o: main.cpp \
+ /usr/share/arduino/hardware/arduino//cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/new.h \
+ libraries/Nunchuk/nunchuk.h
