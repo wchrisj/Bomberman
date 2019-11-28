38e9fce3d294a57190f79bb7084f06a526328528
Spookles <sim.brolsma@gmail.com>
2019-11-28 12:08:30 +0100

    Nunchuk & IR gecombineerd.

--- main.cpp
+++ main.cpp
@@ -9,16 +9,24 @@
 #include <util/delay.h>
 #include <HardwareSerial.h>
 #include "libraries/Nunchuk/nunchuk.h"
+#include "libraries/IRlib/IR.h"
 
 NunchukInput* nunchuk = NunchukInput::getInstance();
 
 int main(void)
 {
 	sei();
-	Serial.begin(9600);
+	Serial.begin(115200);
+	IR ir = IR(3, 38);
+	ir.enableReceiver();
 	while (1)
 	{
 		nunchuk->nunchuk_get();
+		ir.send(0b01, 0x00, 8);
+		ir.enableReceiver();
+		if(ir.decode()) {
+			Serial.print(ir.results.data, HEX);
+		}
 		_delay_ms(100);
 	}
 }
