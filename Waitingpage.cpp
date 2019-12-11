10bb8833dab029c03fc87d8f3027c676002b322a
RoyJansen98 <virtualdj0065@gmail.com>
2019-12-11 10:52:25 +0100

    For loop verwijderd voor het maken van puntjes.

--- Waitingpage.cpp
+++ Waitingpage.cpp
@@ -8,7 +8,6 @@ Waitingpage::Waitingpage(Adafruit_ILI9341 *tft) {
 }
 
 void Waitingpage::show(Adafruit_ILI9341 *tft){                    // Het maken van het wacht scherm
-  //int t = 120;
   tft->fillScreen(ILI9341_BLACK);                                 // maak scherm zwart
 
   tft->setCursor(WP_CURSOR_X60, WP_CURSOR_Y10);                   // zet cursor op x60 en y10
@@ -24,20 +23,16 @@ void Waitingpage::show(Adafruit_ILI9341 *tft){                    // Het maken v
 
 void Waitingpage::cycle(Adafruit_ILI9341 *tft, uint8_t *cycle_staps){ // Het maken van de wacht punten
   uint8_t i = 0;
-  //uint8_t cycle_staps = 0; 
-  for(i = 0; i <= 4; i++) // misschien weg
-  {
     tft->setCursor(*cycle_staps * WP_DOT2DOT_SIZE, WP_CURSOR_Y120);       // zet cursor x(cycle_staps * dot2dot_size) en y120
     tft->setTextColor(ILI9341_YELLOW);  tft->setTextSize(TEXTSIZE4);      // tekst grote en kleur kiezen
     tft->print(".");                                                      // print tekst
-    if(*cycle_staps){                                                     // als cycle_staps géén 0 is dan,
+  if(*cycle_staps){                                                     // als cycle_staps géén 0 is dan,
 
-      tft->setCursor((*cycle_staps-1) * WP_DOT2DOT_SIZE, WP_CURSOR_Y120); //
+    tft->setCursor((*cycle_staps-1) * WP_DOT2DOT_SIZE, WP_CURSOR_Y120); //
       
-    } else{
-      tft->setCursor((MAXDOTS-1) * WP_DOT2DOT_SIZE, WP_CURSOR_Y120);      // anders,
-    }
-    tft->setTextColor(ILI9341_BLACK);  tft->setTextSize(TEXTSIZE4);       // verwijder het voorgande gele puntje     
-    tft->print(".");                                                      // maak punt zwart zoals de achtergrond
+  } else{
+    tft->setCursor((MAXDOTS-1) * WP_DOT2DOT_SIZE, WP_CURSOR_Y120);      // anders,
   }
+  tft->setTextColor(ILI9341_BLACK);  tft->setTextSize(TEXTSIZE4);       // verwijder het voorgande gele puntje     
+  tft->print(".");                                                      // maak punt zwart zoals de achtergrond
 }
