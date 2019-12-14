bee4fc02612680fc43d12251bd5680c874f78123
wchrisj <jansenchris833@gmail.com>
2019-12-14 16:34:47 +0100

    ISR routine verkleind, IR pinnen een define gegeven, if in hoofdloop vervangen door switch .. case, delay voor het zend van een startsignaal weggewerkt

--- bomber.cpp
+++ bomber.cpp
@@ -12,9 +12,13 @@
 #define CHARACTER_MOVE 100
 #define BOMB_EXPLODE 2000
 #define BOMB_EXPLOSION 1500
+#define COUNTER_RESEND_START 100
 #define IR_CHECK 40
 #define LOCAL_PLAYER 0
 #define EXTERN_PLAYER 1
+
+#define IR_RECEIVEPIN PIND2
+#define IR_SENDFREQUENTIE 56
 
 void gameTimerInit();
 void draw();
@@ -27,7 +31,7 @@ volatile char F_readIR = 0;
 volatile short C_readIR = 0;
 volatile char C_charMove = 0;
 volatile short C_bombs[1];
-volatile bool gestart = false;
+volatile uint8_t C_resendStart = 0;
 volatile uint8_t cycle_staps = 0;
 volatile status_t gameStatus;
 volatile Homepage homepage; 			// maak homepage object aan
@@ -55,56 +59,14 @@ ISR(TIMER1_COMPA_vect) { //Elke 15ms
 		F_readIR = 1;
 		C_readIR = 0;
 	}
-	if(gameStatus == playing) {
-		C_charMove++;
-		if(localCharacter.bomb.exists) {
-			C_bombs[LOCAL_PLAYER]++;
-			if(C_bombs[LOCAL_PLAYER] == BOMB_EXPLOSION) {
-				F_bombExplosion[LOCAL_PLAYER] = 1;
-			}
-		}
-		if(externCharacter.bomb.exists) {
-			C_bombs[1]++;
-			if(C_bombs[1] == BOMB_EXPLOSION) {
-				F_bombExplosion[1] = 1;
-			}
-		}
-		if(C_charMove == CHARACTER_MOVE) { //200ms (100ticks * 2ms = 200ms)
-			C_charMove = 0;
-			if (nunchuk->status.UP == 1) {
-				localCharacter.move(Character::UP);
-				Serial.println("UP");
-			}
-			else if (nunchuk->status.RIGHT == 1) {
-				localCharacter.move(Character::RIGHT);
-				Serial.println("RIGHT");
-			}
-			else if (nunchuk->status.DOWN == 1) {
-				localCharacter.move(Character::DOWN);
-				Serial.println("DOWN");
-			}
-			else if (nunchuk->status.LEFT == 1) {
-				localCharacter.move(Character::LEFT);
-				Serial.println("LEFT");
-			}
-			if (nunchuk->status.Z == 1) {
-				if(!localCharacter.bomb.exists) {
-					localCharacter.bomb.placeBomb(localCharacter.x, localCharacter.y);
-				}
-			}
-			draw();
-		}
+	switch(gameStatus) {
+		case waiting:
+			C_resendStart++;
+			break;
 
-		if (C_bombs[LOCAL_PLAYER] == BOMB_EXPLODE) { //4seconden
-			C_bombs[LOCAL_PLAYER] = 0;
-			F_bombExplosion[LOCAL_PLAYER] = 0;
-			localCharacter.bomb.explodeBomb();
-		}
-		if (C_bombs[EXTERN_PLAYER] == BOMB_EXPLODE) {
-			C_bombs[EXTERN_PLAYER] = 0;
-			F_bombExplosion[EXTERN_PLAYER] = 0;
-			externCharacter.bomb.explodeBomb();
-		}
+		case playing:
+			C_charMove++;
+			break;
 	}
 }
 
@@ -115,7 +77,7 @@ int main (void)
 
 	gameStatus = notReady;	// status beginnen met notReady
 	uint8_t identifierReceived = 0;
-	IR ir = IR(2, 56);
+	IR ir = IR(IR_RECEIVEPIN, IR_SENDFREQUENTIE);
 	ir.enableReceiver();
 
 	homepage.HomepageText(&tft);
@@ -134,49 +96,102 @@ int main (void)
 		}
 
 		if(F_readIR == 1) {
-			if( ir.decode()){			// IR uitlezen
+			if(ir.decode()){			// IR uitlezen
 				identifierReceived = 1;	// identifierReceived aanzetten
 				ir.resumeReceiver();	// gaat volgende signaal lezen
 			}
 			F_readIR = 0;
 		}
 
-		if (gameStatus == notReady){					// als status is notReady, dan
-			if (nunchuk->status.C == 1){			// kijken of de Z is ingedrukt met de Nunchuk
-				if(!gestart) {						// als niet gestart, dan
-					gestart = true;					// zet gestart op true
+		switch(gameStatus){
+			case notReady:								// als status is notReady, dan
+				if (nunchuk->status.C == 1){			// kijken of de Z is ingedrukt met de Nunchuk
+					gameStatus = waiting; 				// zet status op waiting
+					waitingpage.show(&tft);				// maak het wachtscherm
+				}else {
 					continue;
 				}
-				gameStatus = waiting; 					// zet status op waiting
-				waitingpage.show(&tft);				// maak het wachtscherm
-			}else {
-				continue;
-			}
-		}
-		if(gameStatus == waiting){									// Als de status waiting is, dan
-			// Serial.println("Waiting");
-			if(identifierReceived){									// De ontvangen data decoderen zodat er iets mee gedaan kan worden
-				if(ir.results.identifier == IDENTIFIER_START){ 	// Als het IR signaal een start identifier stuurt
-				// Serial.println("IDENTIFIER");
-					gameStatus = playing;							// dan, Zet status om naar playing
-					lcd.fillScreen(BG_COLOR);
-					mapGenerator.createMap(0xFFFF);
-					lcd.drawMap();
-					localCharacter.init(16, 16, ILI9341_YELLOW);
-					lcd.statusBar();
-				}
-			} else{ 
-				_delay_ms(100); //DIT MOET EEN GAME TICK WORDEN #################################################
-				cycle_staps++;							// cycle_staps verhogen met +1
-				if (cycle_staps == MAXDOTS){			// Als cycle_staps de maximale aantal punten heeft geprint 
-					cycle_staps = 0;					// dan, cycle_staps resetten naar 0
-				}
-				waitingpage.cycle(&tft, &cycle_staps);	// functie printen van dots
-				ir.send(IDENTIFIER_START,0,0);			// Verzend IR signaal met alleen een identifier
+				break;
+			
+			case waiting:											// Als de status waiting is, dan
+				if(identifierReceived){								// De ontvangen data decoderen zodat er iets mee gedaan kan worden
+					if(ir.results.identifier == IDENTIFIER_START){ 	// Als het IR signaal een start identifier stuurt
+						gameStatus = playing;						// dan, Zet status om naar playing
+						lcd.fillScreen(BG_COLOR);
+						mapGenerator.createMap(0xFFFF);
+						lcd.drawMap();
+						localCharacter.init(16, 16, ILI9341_YELLOW);
+						lcd.statusBar();
+					}
+				} else if(C_resendStart == COUNTER_RESEND_START){ // Opnieuw een startsignaal versturen?
+					C_resendStart = 0; 
+					cycle_staps++;							// cycle_staps verhogen met +1
+					if (cycle_staps == MAXDOTS){			// Als cycle_staps de maximale aantal punten heeft geprint 
+						cycle_staps = 0;					// dan, cycle_staps resetten naar 0
+					}
+					waitingpage.cycle(&tft, &cycle_staps);	// functie printen van dots
+					ir.send(IDENTIFIER_START,0,0);			// Verzend IR signaal met alleen een identifier
 
-				ir.enableReceiver();					// Zet de IR-ontvanger aan
-				continue;
-			}
+					ir.enableReceiver();					// Zet de IR-ontvanger aan
+					continue;
+				}else{
+					continue;
+				}
+				break;
+			
+			case playing:
+				// Player bewegingen
+				if(C_charMove == CHARACTER_MOVE) { //200ms (100ticks * 2ms = 200ms)
+					C_charMove = 0;
+					if (nunchuk->status.UP == 1) {
+						localCharacter.move(Character::UP);
+						Serial.println("UP");
+					}
+					else if (nunchuk->status.RIGHT == 1) {
+						localCharacter.move(Character::RIGHT);
+						Serial.println("RIGHT");
+					}
+					else if (nunchuk->status.DOWN == 1) {
+						localCharacter.move(Character::DOWN);
+						Serial.println("DOWN");
+					}
+					else if (nunchuk->status.LEFT == 1) {
+						localCharacter.move(Character::LEFT);
+						Serial.println("LEFT");
+					}
+					if (nunchuk->status.Z == 1) {
+						if(!localCharacter.bomb.exists) {
+							localCharacter.bomb.placeBomb(localCharacter.x, localCharacter.y);
+						}
+					}
+					draw();
+				}
+
+				// Bommen
+				if(localCharacter.bomb.exists) {
+					C_bombs[LOCAL_PLAYER]++;
+					if(C_bombs[LOCAL_PLAYER] == BOMB_EXPLOSION) {
+						F_bombExplosion[LOCAL_PLAYER] = 1;
+					}
+				}
+				if(externCharacter.bomb.exists) {
+					C_bombs[EXTERN_PLAYER]++;
+					if(C_bombs[EXTERN_PLAYER] == BOMB_EXPLOSION) {
+						F_bombExplosion[EXTERN_PLAYER] = 1;
+					}
+				}
+				
+				if (C_bombs[LOCAL_PLAYER] == BOMB_EXPLODE) { //4seconden
+					C_bombs[LOCAL_PLAYER] = 0;
+					F_bombExplosion[LOCAL_PLAYER] = 0;
+					localCharacter.bomb.explodeBomb();
+				}
+				if (C_bombs[EXTERN_PLAYER] == BOMB_EXPLODE) {
+					C_bombs[EXTERN_PLAYER] = 0;
+					F_bombExplosion[EXTERN_PLAYER] = 0;
+					externCharacter.bomb.explodeBomb();
+				}
+			break;
 		}
 	}
 
@@ -185,16 +200,12 @@ int main (void)
 
 //Initalizeer timer1 voor de gameclock naar Compare A register elke 2ms
 void gameTimerInit() {
-	TCCR1A = 0; // set entire TCCR1A register to 0
-	TCCR1B = 0; // same for TCCR1B
-	TCNT1  = 0; // initialize counter value to 0
-	// set compare match register for 500 Hz increments
-	OCR1A = 31999; // = 16000000 / (1 * 500) - 1 (must be <65536)
-	// turn on CTC mode
+	TCCR1A = 0; 
+	TCCR1B = 0; 
+	TCNT1  = 0;
+	OCR1A = 31999;
 	TCCR1B |= (1 << WGM12);
-	// Set CS12, CS11 and CS10 bits for 1 prescaler
-	TCCR1B |= (0 << CS12) | (0 << CS11) | (1 << CS10);
-	// enable timer compare interrupt
+	TCCR1B |= (1 << CS10);
 	TIMSK1 |= (1 << OCIE1A);
 }
 
@@ -206,7 +217,6 @@ void draw() {
 	}
   
 	lcd.drawPlayer(localCharacter.x / BLOCK_SIZE, localCharacter.y / BLOCK_SIZE, PLAYER_1); // mss later eerst nieuwe tekenen en dan pas oude weghalen
-	//tft.fillRect(localCharacter.x, localCharacter.y, localCharacter.height, localCharacter.width, ILI9341_YELLOW);
 	//Tekent de bom van localCharacter op het scherm
 
 	if(localCharacter.bomb.exists == true) {
